7881f9c23fbf58531b7def60522a0503f9a6ff29
noo-dev <noo.dev23@gmail.com>
2024-01-17 14:27:39 +0500

    refactored from mutex to atomic

--- race_test.go
+++ race_test.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"sync"
+	"sync/atomic"
 	"testing"
 )
 
 func TestDataRaceConditions(t *testing.T) {
 	var state int32
-	var mu sync.RWMutex
 
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			mu.Lock()
-			state += int32(i)
-			mu.Unlock()
+			atomic.AddInt32(&state, int32(i))
 		}(i)
 	}
 
