3cbfa7abd81edfed38fbe3b36685f7e26d45de21
noo-dev <noo.dev23@gmail.com>
2024-01-17 14:18:34 +0500

    test refactored to mutexes

--- race_test.go
+++ race_test.go
@@ -1,13 +1,19 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestDataRaceConditions(t *testing.T) {
 	var state int32
+	var mu sync.RWMutex
 
 	for i := 0; i < 10; i++ {
 		go func(i int) {
+			mu.Lock()
 			state += int32(i)
+			mu.Unlock()
 		}(i)
 	}
 
